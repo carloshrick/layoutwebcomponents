a8ce276336f93892a21a3c1f83172fd274fbf6e5
Carlos Henrique De Souza <125037239+carloshrick@users.noreply.github.com>
2023-10-06 10:21:24 -0300

    Add files via upload

---
+++ nobruapelao/build/Produtos.js
@@ -0,0 +1,41 @@
+class Produtos {
+    id;
+    descricao;
+    preco;
+    imagem;
+    constructor(id, desc, img, preco) {
+        this.id = id;
+        this.descricao = desc;
+        this.imagem = img;
+        this.preco = preco;
+    }
+    getDescricao() {
+        return this.descricao;
+    }
+    getImagem() {
+        return this.imagem;
+    }
+    getPreco() {
+        return this.preco;
+    }
+}
+export let lista_objetos = [
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00),
+    new Produtos(1, "Sandalha do spaider men", "https://clovisatacado.vteximg.com.br/arquivos/ids/228855-507-507/Papete-Infantil-Masculina-Homem-Aranha-Grendene-Kids-21852-3291852_018-01.jpg?v=636825560883270000", 300.00)
+];
